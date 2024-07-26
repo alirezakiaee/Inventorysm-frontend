915b8251fea75357bb092fb2f536919e71642882
akphisher <alirezakiaee91@gmail.com>
2024-07-26 14:59:09 -0400

    added UI components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Inventorysm</h1>
+    <main className="flex flex-col items-center justify-between p-24">
+      <h1 className="text-6xl font-bold">Inventorysm</h1>
+      <Button>Get Started</Button>
     </main>
   );
 }
