35b97b1cade86d96fef6e8fb90acfab3ce85350f
alirezakiaee <alirezakiaee91@gmail.com>
2024-07-27 19:00:35 -0400

    added the profile image from google account

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Tasks from "@/components/inventorysm/tasks";
+import UserProfile from "@/components/inventorysm/user-profile";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold">Inventorysm</h1>
-      <Button>Get Started</Button>
+      <h1 className="text-6xl font-bold">Dashboard</h1>
+      <UserProfile />
       <Tasks />
     </main>
   );
