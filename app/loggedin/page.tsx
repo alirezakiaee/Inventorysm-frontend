5b98678e5b11c5948a6ec2ed7b23cc2d41e84554
alirezakiaee <alirezakiaee91@gmail.com>
2024-07-27 18:22:52 -0400

    added Google Oauth

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,15 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Tasks from "@/components/inventorysm/tasks";
-import { signInAction } from "@/actions/auth-action";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold">Inventorysm</h1>
       <Button>Get Started</Button>
-      <form action={signInAction}>
-        <Button type="submit">Sign In</Button>
-      </form>
+      <Tasks />
     </main>
   );
 }
