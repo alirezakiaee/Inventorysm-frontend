8024622ec1f21d50c4f52b90450b2c31a96119dc
alirezakiaee <alirezakiaee91@gmail.com>
2024-07-28 23:23:37 -0400

    added some auth setups

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,41 @@
 "use client";
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import { ReactNode } from "react";
+import { ReactNode, useMemo } from "react";
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-export function Providers({ children, session}: { children: ReactNode, session : Session | null }) {
-  return <SessionProvider session={session}><ConvexProvider client={convex}>{children}</ConvexProvider></SessionProvider>;
+function useAuth() {
+  const { data: session, update } = useSession();
+  return useMemo(() => ({
+    isLoading: false,
+    isAuthenticated: session !== null,
+    fetchAccessToken: async ({ forceRefreshToken }: { forceRefreshToken: boolean }) => {
+      if (forceRefreshToken) {
+        const session = await update();
+        return session?.convexToken ?? null;
+    }
+  },
+  }),
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  [JSON.stringify(session?.user)]
+);
+}
+
+export function Providers({
+  children,
+  session,
+}: {
+  children: ReactNode;
+  session: Session | null;
+}) {
+  return (
+    <SessionProvider session={session}>
+      <ConvexProvider client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProvider>
+    </SessionProvider>
+  );
 }
--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as authAdapter from "../authAdapter.js";
 import type * as tasks from "../tasks.js";
 
 /**
@@ -25,6 +26,7 @@ import type * as tasks from "../tasks.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  authAdapter: typeof authAdapter;
   tasks: typeof tasks;
 }>;
 export declare const api: FilterApi<
