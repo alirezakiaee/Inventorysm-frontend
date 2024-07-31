56e7e5c86858705c6d9d182ee63cc065015b48be
akphisher <alirezakiaee91@gmail.com>
2024-07-31 15:18:11 -0400

    adjusted some styles on user-profile

--- components/nav/side-bar.tsx
+++ components/nav/side-bar.tsx
@@ -32,14 +32,11 @@ export default function SideBar() {
           Inventorysm
         </div>
         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-          <Link
-            href="/loggedin"
-            className="flex items-center gap-2 font-semibold"
-          >
-            <span className="w-10 h-10">
-              <UserProfile />
-            </span>
-          </Link>
+          {/* <Link href="#" className="flex items-center gap-2 font-semibold"> */}
+          <span className="w-10 h-10">
+            <UserProfile />
+          </span>
+          {/* </Link> */}
         </div>
         <div className="flex-1">
           <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
--- components/nav/user-profile.tsx
+++ components/nav/user-profile.tsx
@@ -1,65 +1,3 @@
-// "use client";
-// import Image from "next/image";
-// import React from "react";
-// import { useSession } from "next-auth/react";
-// import { Button } from "@/components/ui/button";
-// import {
-//   DropdownMenu,
-//   DropdownMenuContent,
-//   DropdownMenuGroup,
-//   DropdownMenuItem,
-//   DropdownMenuLabel,
-//   DropdownMenuPortal,
-//   DropdownMenuSeparator,
-//   DropdownMenuShortcut,
-//   DropdownMenuSub,
-//   DropdownMenuSubContent,
-//   DropdownMenuSubTrigger,
-//   DropdownMenuTrigger,
-// } from "@/components/ui/dropdown-menu";
-// export default function UserProfile() {
-//   const session = useSession();
-//   const imageUrl = session.data?.user?.image!;
-//   const name = session.data?.user?.name!;
-//   const email = session.data?.user?.email!;
-
-//   return (
-//     // <div className="flex items-center justify-start gap-1 lg:gap-2 m-0 p-0 lg:px-3 lg:w-full">
-//     <DropdownMenu>
-//       <DropdownMenuTrigger asChild>
-//         <div className="flex items-center justify-start gap-1 lg:gap-2 m-0 p-0 lg:w-full">
-//           <Button variant="ghostt">
-//             {imageUrl && (
-//               <Image
-//                 src={imageUrl}
-//                 className="rounded-full ml-0 mr-2"
-//                 alt={`${name} profile picture`}
-//                 width={50}
-//                 height={50}
-//               />
-//             )}
-//             <div className="flex flex-col items-start justify-center">
-//               <span className="text-sm font-semibold text-gray-800">
-//                 {name}
-//               </span>
-//               <span className="text-xs text-gray-500">{email}</span>
-//             </div>
-//           </Button>
-//         </div>
-//       </DropdownMenuTrigger>
-//       <DropdownMenuContent className="w-56">
-//         <DropdownMenuLabel>My Account</DropdownMenuLabel>
-//         <DropdownMenuSeparator />
-//         <DropdownMenuGroup>
-//           <DropdownMenuItem className="lg:w-full px-28 justify-evenly">
-//             Profile
-//             <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-//           </DropdownMenuItem>
-//         </DropdownMenuGroup>
-//       </DropdownMenuContent>
-//     </DropdownMenu>
-//   );
-// }
 "use client";
 import Image from "next/image";
 import React from "react";
@@ -75,6 +13,7 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { signOutAction } from "@/actions/auth-action";
 
 export default function UserProfile() {
   const session = useSession();
@@ -110,7 +49,15 @@ export default function UserProfile() {
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
           <DropdownMenuItem className="lg:w-full text-left px-4 py-2">
-            Logout
+            <form action={signOutAction}>
+              <Button
+                variant="ghostt"
+                type="submit"
+                className="hover:text-primary"
+              >
+                Sign Out
+              </Button>
+            </form>
             <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
           </DropdownMenuItem>
         </DropdownMenuGroup>
